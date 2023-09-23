364ca128960fe4b0a4335849e20db1c9b1e972a7
doananhkiet886 <doananhkiet886@gmail.com>
2023-09-23 13:27:31 +0700

    fix eslint

--- src/api/v1/models/productModel.js
+++ src/api/v1/models/productModel.js
@@ -1,4 +1,4 @@
-const { model, Schema, Types } = require('mongoose')
+const { model, Schema } = require('mongoose')
 
 const PRODUCT_COLLECTION_NAME = 'Product'
 const PRODUCT_MODEL_NAME = 'productModel'
