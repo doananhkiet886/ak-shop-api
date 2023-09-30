4b4212e93f8b52e278fe80fd472741499888b1af
doananhkiet886 <doananhkiet886@gmail.com>
2023-09-30 11:36:06 +0700

    [ADD] discount model

---
+++ src/api/v1/models/discountModel.js
@@ -0,0 +1,29 @@
+const { model, Schema } = require('mongoose')
+
+const COLLECTION_NAME = 'discounts'
+const DOCUMENT_NAME = 'discount'
+
+const discountSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  type: { type: String, default: 'fixed amount' }, // percentage
+  value: { type: Number, required: true },
+  code: { type: String, required: true, unique: true },
+  startDate: { type: Date, required: true },
+  endDate: { type: Date, required: true },
+  maxUses: { type: Number, required: true },
+  usesCount: { type: Number, default: 0 },
+  userUsed: { type: Array, default: [] },
+  maxUsesPerUser: { type: Number, required: true },
+  minOrderValue: { type: Number, required: true },
+  shop: { type: Schema.Types.ObjectId, required: true, ref: 'shop' },
+  isActive: { type: Boolean, default: true },
+  appliesTo: { type: String, required: true, enum: ['all', 'specific'] },
+  productIds: { type: Array, default: [] }
+}, {
+  timestamps: true,
+  versionKey: false,
+  collection: COLLECTION_NAME
+})
+
+module.exports = model(DOCUMENT_NAME, discountSchema)
